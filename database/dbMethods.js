7855015167928544f2198bdf1ceb9fa552d8dc3d
CorruptEquation <ovidiu.valentin@protonmail.com>
2025-12-17 15:00:44 +0200

    Promisifying db run methods

--- database/dbMethods.js
+++ database/dbMethods.js
@@ -5,49 +5,43 @@ sqlite3.verbose();
 
 const dbName = process.env.DB_NAME;
 
+// Promisifying methods
+function dbRun(db, sql, okMessage) {
+  return new Promise((resolve, rej) => {
+      db.run(
+        sql,
+        (e) => {
+          if (e) rej(e);
+          else {
+            console.log(okMessage)
+            resolve();
+          };
+        }
+      );
+    })
+}
+
+// Open-Close db wrapper
 function useDb(callback) {
   return function () {
-    const db = new sqlite3.Database(dbName, (e) => {
+    const db = new sqlite3.Database(dbName, async(e) => {
       if (e) {
         console.log(e.message);
         return;
       }
 
-      callback(db, () => db.close());
+      await callback(db);
+      db.close();
     });
   }
 }
 
-export const dbInit = useDb(async (db, done) => {
+export const dbInit = useDb(async (db) => {
   try {
     console.log("Connected to database");
 
-    await new Promise((response, rej) => {
-      db.run(
-        initRecipes,
-        (e) => {
-          if (e) rej(e);
-          else {
-            console.log("Table recipes created or existed")
-            response();
-          };
-        }
-      );
-    })
+    await dbRun(db, initRecipes, "Table recipes created or existed");
     
-    await new Promise((response, rej) => {
-      db.run(
-        initUsers,
-        (e) => {
-          if (e) rej(e);
-          else {
-            console.log("Table users created or existed");
-            response();
-          }
-        }
-      );
-    })
-
-    done();
+    await dbRun(db, initUsers, "Table users created or existed");
   } catch(e) { console.log(e) }
 });
