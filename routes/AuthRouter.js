8c8c8c6bdb70353e9dac4b3a47645c6425ebd56d
CorruptEquation <ovidiu.valentin@protonmail.com>
2026-01-05 00:25:01 +0200

    Code refactor

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,59 +1,52 @@
+// TODO: Create posts and post routes
+// TODO: Send JSON message with status codes
+// TODO: Rewrite source code in TypeScript
+// TODO: Rate limiting
+
 import express from "express";
 import bcrypt from "bcrypt";
 
 import { dbGetUser, dbInsertUser, dbRemUser } from "../database/dbMethods.js";
-import { genAccessToken, genRefreshToken, verifyRefreshTk } from "../utils/jwtMethods.js";
+import { verifyRefreshTk } from "../utils/jwtMethods.js";
 import { encryptDeterministic } from "../utils/aesMethods.js";
-import { getAPIToken } from "../utils/apiTokenMethods.js"
 import { redis } from "../redis.js";
+import { resBody } from "../utils/utils.js";
 
 export const router = express.Router();
 
-// TODO: Separate signup and login and post endpoints
-router.post("/api/auth", async (req, res) => {
+router.post("/api/signup", async (req, res) => {
   try {
-    const { email, password, mode } = req.body;
-    const resJsonObj = {};
-
-    // Bad requests
-    if (!["signup", "login"].includes(mode)) return res.sendStatus(400);
-    // TODO: Email and pw format validity (client should handle bad formats before sending the request on login/signup. On login, the error message should just say that all account creds are incorrect (400) if at least one invalid format is present. Formats need to be specified in API docs).
-
-    // TODO: Create posts and post routes
-    // TODO: Send JSON message with status codes
-    // TODO: Rewrite source code in TypeScript
-    // TODO: Rate limiting
+    const { email, password } = req.body;
+    
+    // TODO: Bad requests. Email and pw format validity (client should handle bad formats before sending the request on login/signup. On login, the error message should just say that all account creds are incorrect (400) if at least one invalid format is present. Formats need to be specified in API docs).
 
     const encryptedEmail = encryptDeterministic(email, "email");
     const user = await dbGetUser(encryptedEmail);
+    if (user) return res.status(409).json({ "Response": "Account already attached to this email" });
+    const hashPw = await bcrypt.hash(password, 12);
+    await dbInsertUser([encryptedEmail, hashPw]);
 
-    if (mode === "signup") {
-      if (user) return res.sendStatus(409); // Conflict
+    return res.status(201).json(await resBody(encryptedEmail, "Account created successfully"));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+})
 
-      const hashPw = await bcrypt.hash(password, 12);
+router.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-      await dbInsertUser([encryptedEmail, hashPw]);
-      
-      res.status(201); // Created
-    } else if (mode === "login") {
-      if (!user) return res.sendStatus(400); // No account
+    // TODO: Bad requests. Email and pw format validity (client should handle bad formats before sending the request on login/signup. On login, the error message should just say that all account creds are incorrect (400) if at least one invalid format is present. Formats need to be specified in API docs).
 
-      const validPw = await bcrypt.compare(password, user.password);
-      if (!validPw) return res.sendStatus(400); // Wrong creds
-      
-      const APIToken = await getAPIToken(encryptedEmail);
-      if(APIToken) resJsonObj.APIToken = APIToken;
+    const encryptedEmail = encryptDeterministic(email, "email");
+    const user = await dbGetUser(encryptedEmail);
+    if (!user) return res.status(400).json({ "Response": "Invalid credentials" });
 
-      res.status(200); // OK
-    }
-    resJsonObj.accessToken = genAccessToken(encryptedEmail);
+    const validPw = await bcrypt.compare(password, user.password);
+    if (!validPw) return res.status(400).json({ "Response": "Invalid credentials" });
 
-    const refreshToken = genRefreshToken(encryptedEmail);
-    resJsonObj.refreshToken = refreshToken;
-    await redis.set(refreshToken, "refresh", { EX: process.env.REFRESH_TOKEN_EXP_SEC }); // Add refresh token to cache
-
-    res.json(resJsonObj);
-    return res.send();
+    return res.status(200).json(await resBody(encryptedEmail, "Login successful"));
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal server error" });
@@ -64,11 +57,11 @@ router.post("/api/auth", async (req, res) => {
 router.delete("/api/logout", async (req, res) => {
   try {
     const refreshTk = req.body.token;
-    if (!refreshTk) return res.sendStatus(400);
+    if (!refreshTk) return res.status(400).json({ "Response": "No token inside request body" });
 
     await redis.del(refreshTk);
     
-    return res.sendStatus(200);
+    return res.status(200).json({ "Response": "Logout successful" });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal server error" });
@@ -79,14 +72,14 @@ router.delete("/api/logout", async (req, res) => {
 router.delete("/api/account", async (req, res) => {
   try {
     const refreshTk = req.body.token;
-    if (!refreshTk) return res.sendStatus(400);
+    if (!refreshTk) return res.status(400).json({ "Response": "No token inside request body" });
 
     await redis.del(refreshTk);
 
-    try{ dbRemUser(verifyRefreshTk(refreshTk)); }
+    try { await dbRemUser(verifyRefreshTk(refreshTk)); }
     catch(e) { return }
 
-    return res.sendStatus(200);
+    return res.status(200).json({ "Response": "Account deleted successfully" });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal server error" });
